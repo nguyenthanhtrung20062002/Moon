2d9cd85ba1a8b701bc1e4fa1e59dcbf033ea5c07
nguyenthanhtrung20062002 <trungthanthien99@gmail.com>
2024-02-24 16:49:37 +0700

    loginAc

--- app/src/main/java/com/example/moon/login/LoginActivity.java
+++ app/src/main/java/com/example/moon/login/LoginActivity.java
@@ -1,15 +1,17 @@
-package com.example.moon;
+package com.example.moon.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.moon.R;
+
+public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
 }
