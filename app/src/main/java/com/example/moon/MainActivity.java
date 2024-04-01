9602e64f3d566d4fe5773ea1aa2b64db3b2f530b
GiaThinh <thinhdo1509@gmail.com>
2024-04-01 12:59:08 +0700

    explore, book read, audio player

--- app/src/main/java/com/example/moon/MainActivity.java
+++ app/src/main/java/com/example/moon/MainActivity.java
@@ -1,15 +1,47 @@
 package com.example.moon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.moon.activities.AudioPlayerActivity;
+import com.example.moon.fragment.BookDetailFragment;
+import com.example.moon.fragment.ExploreFragment;
+import com.example.moon.fragment.HomeFragment;
+import com.example.moon.fragment.PaymentFragment;
+import com.example.moon.fragment.login.LoginEmailFragment;
+import com.example.moon.fragment.login.LoginPassFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bt1, bt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt1 = findViewById(R.id.bt1);
+        bt2 = findViewById(R.id.bt2);
 
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AudioPlayerActivity.class);
+                startActivity(intent);
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bt2.setVisibility(View.GONE);
+                Fragment fragment = new ExploreFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.activity, fragment).commit();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/moon/fragment/ExploreFragment.java
+++ app/src/main/java/com/example/moon/fragment/ExploreFragment.java
@@ -9,9 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.moon.R;
-public class PaymentFragment extends Fragment {
+
+public class ExploreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_payment, container, false);
+        return inflater.inflate(R.layout.fragment_explore, container, false);
     }
 }
